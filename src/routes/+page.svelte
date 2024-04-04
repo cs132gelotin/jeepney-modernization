486e6744c757ed2beedb5e4dbd20dd1234314fbf
Angelo Convento <convento.angelo@gmail.com>
2024-04-04 23:18:09 +0800

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+</script>
+
+<main>
+	<div class="container mx-auto"></div>
+	<div class="relative flex h-full w-full flex-col items-start">
+		<section
+			class="mx-auto flex min-h-screen max-w-[980px] flex-col items-center justify-center gap-2 py-24 pb-20 text-center"
+		>
+			<h1 class="leading-1.1 text-6xl font-bold tracking-tighter">
+				#JeepneyNation: Sentiment towards Modernization
+			</h1>
+			<p class="mt-6 max-w-[750px] text-lg text-muted-foreground">
+				A Twitter analysis of Filipino reception towards Jeepney Modernization
+			</p>
+		</section>
+		<section class="mx-auto flex max-w-[980px] flex-col items-center gap-2 py-24 pb-20 text-center">
+			<h1 class="text-4xl font-extrabold tracking-tight">Overview</h1>
+		</section>
+	</div>
+</main>
